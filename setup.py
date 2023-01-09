f8c2eb9779d212323d7b588f3ea855e7096d3608
Oren Tirosh <orent@hishome.net>
2023-01-09 10:03:14 +0000

    0.2 - universal

--- setup.py
+++ setup.py
@@ -3,7 +3,9 @@
 from distutils.core import setup
 setup(
     name='magicmain',
-    version='0.1',
+    version='0.2',
+    python_requires='>=2.7',
+    options={'bdist_wheel':{'universal':'1'}},
     description='Make appendable dependency zips',
     author='Oren Tirosh',
     author_email='orent@hishome.net',
